a0429a5d0e17b7f943ff4226ae38b06a17f1706e
Atif-Bashir-1998 <adobe8832289@gmail.com>
2023-01-15 10:42:09 +0500

    REST API has been created and is linked with mongodb

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 // establish a connection with mongodb
 const uri =
 	"mongodb+srv://dauris-dude:dyWNzqsGVhErKgwX@cluster0.sz0fk0v.mongodb.net/?retryWrites=true&w=majority"; //uri for mongodb
-const client = new MongoClient(uri);
+var client = new MongoClient(uri);
 
 async function connect() {
   // try to establish a connection with the mongodb
@@ -10,37 +10,55 @@ async function connect() {
     await client.connect().then(() => console.log("connected to mongodb"));
   } catch (e) {
     console.log("error while connecting to mongodb", e);
-  } finally {
-    await client.close(); // if there are errors, close the client's connection to mongodb
   }
 }
 
 connect();
 
-// get all lessons
+// defining all the functions responsible for contacting mongodb and doing database transactions
+async function createOrder(order) {
+	return await client.db("project").collection("orders").insertOne(order);
+}
+
+async function getLessons() {
+	return client
+    .db("project")
+    .collection("lessons")
+    .find().toArray();
+}
+
+async function updateLesson(id, space) {
+	return await client
+    .db("project")
+    .collection("lessons")
+    .updateOne({ _id: ObjectId(id) }, { $inc: { "space": -space } });
+}
+
+// setting up express server
 const express = require("express");
 
 const app = express();
 app.use(express.json());
 
 // Defining api routes
-app.get("/api/lesson", (req, res) => {
+app.get("/api/lesson", async (req, res) => {
+  const result = await getLessons();
   res.send({
-    msg: "load all the lessons"
+    result,
   });
 });
 
-app.post("/api/order", (req, res) => {
+app.post("/api/order", async (req, res) => {
+	const result = await createOrder(req.body);
   res.send({
-		msg: "create a new order",
-		sentData: req.body
+		msg: `New order with id [${result.insertedId}] has been created successfully!`,
 	});
 });
 
-app.put("/api/lesson/:id", (req, res) => {
+app.put("/api/lesson/:id", async(req, res) => {
+	const result = await updateLesson(req.params.id, req.body.space);
 	res.send({
-    msg: `update spaces in lesson with id: {${req.params.id}}`,
-    sentData: req.body,
+    msg: `Spaces in the lesson [id: ${req.params.id}] updated after successful order`,
   });
 });
 
